6bc6c41c2ce4667041940ea2b882e67398f7e6b4
HANATANI Takuma <mail@potato4d.me>
2018-08-26 19:44:59 +0900

    Add proxy module example

---
+++ packages/modules-proxy-example/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        modules-proxy-example
+      </h1>
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          {{post.title}}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async mounted() {
+    this.posts = await this.$axios.$get('/api/posts')
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 16px;
+}
+ul {
+  margin: 16px 0;
+}
+</style>
+
